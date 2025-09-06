a9ef9b2ba76b29398ef72d47d80b008789004a45
alexmo16 <alexismoreldev@gmail.com>
2025-09-06 16:53:34 -0400

    test

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -2,6 +2,7 @@ use std::io::{ Read, Write };
 use std::net::TcpStream;
 use std::thread;
 
+//test commit
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stream = TcpStream::connect("127.0.0.1:8080")?;
     println!("Connected to server");
